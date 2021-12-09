2b465e655bede91b4b4266213c32a2f27196e0f5
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2021-12-09 23:45:25 +0100

    feat(cli): ⬆️upgrade of cli lib to v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/uded/docker-health-check
 
 go 1.13
 
-require github.com/urfave/cli v1.20.0
+require github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -7,70 +7,66 @@ import (
 	"strings"
 	"time"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-
-	app := cli.NewApp()
-	app.Version = "0.0.4"
-	app.Name = "Health Checker"
-	app.Usage = "Hits an endpoint for you.  healthcheck -url=http://localhost/ping"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "url, U",
-			Usage:       "the url to hit (required)",
-			EnvVar:      "HEALTHCHECK_URL",
-			Destination: &url,
+	app := &cli.App{
+		Version: "0.0.5",
+		Name:    "Health Checker",
+		Usage:   "Hits an endpoint for you.  healthcheck http://localhost/ping",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:  "headers, H",
+				Usage: "specify a header and value for the request (-H=key:value)",
+			},
+			&cli.StringFlag{
+				Name:        "verb, V",
+				Usage:       "the HTTP verb to use",
+				Value:       "GET",
+				EnvVars:     []string{"HEALTHCHECK_VERB"},
+				Destination: &httpVerb,
+			},
+			&cli.IntFlag{
+				Name:        "code, C",
+				Usage:       "expected response code",
+				Value:       http.StatusOK,
+				EnvVars:     []string{"HEALTHCHECK_RESPONSECODE"},
+				Destination: &statusCode,
+			},
+			&cli.IntFlag{
+				Name:        "timeout, T",
+				Usage:       "timeout for HTTP connection",
+				Value:       0,
+				EnvVars:     []string{"HEALTHCHECK_TIMEOUT"},
+				Destination: &timeOut,
+			},
+			// http body not supported yet
+			// response body checking not supported yet
 		},
-		cli.StringSliceFlag{
-			Name:  "headers, H",
-			Usage: "specify a header and value for the request (-H=key:value)",
-		},
-		cli.StringFlag{
-			Name:        "verb, V",
-			Usage:       "the HTTP verb to use",
-			Value:       "GET",
-			EnvVar:      "HEALTHCHECK_VERB",
-			Destination: &httpVerb,
-		},
-		cli.IntFlag{
-			Name:        "code, C",
-			Usage:       "expected response code",
-			Value:       http.StatusOK,
-			EnvVar:      "HEALTHCHECK_RESPONSECODE",
-			Destination: &statusCode,
-		},
-		cli.IntFlag{
-			Name:        "timeout, T",
-			Usage:       "timeout for HTTP connection",
-			Value:       0,
-			EnvVar:      "HEALTHCHECK_TIMEOUT",
-			Destination: &timeOut,
-		},
-		// http body not supported yet
-		// response body checking not supported yet
+		Action: actionFunc,
 	}
-	app.Action = actionFunc
+	// app.Action, = actionFunc
 
 	app.Run(os.Args)
 }
 
 func actionFunc(c *cli.Context) error {
-	if len(url) < 0 {
-		return cli.NewExitError("url length must be > 0 ", 1)
+	url = c.Args().Get(0)
+	if len(url) == 0 {
+		return cli.Exit("url length must be > 0 ", 1)
 	}
 
 	req, err := http.NewRequest(httpVerb, url, nil)
 	if err != nil {
-		return cli.NewExitError(err.Error(), 1)
+		return cli.Exit(err.Error(), 1)
 	}
 	for _, str := range c.StringSlice("headers") {
 		kv := strings.Split(str, ":")
 		if len(kv) == 2 {
 			req.Header.Add(kv[0], kv[1])
 		} else {
-			return cli.NewExitError("header field must be in the format \"key:value\"", 1)
+			return cli.Exit("header field must be in the format \"key:value\"", 1)
 		}
 	}
 	req.Close = true
@@ -84,7 +80,7 @@ func actionFunc(c *cli.Context) error {
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return cli.NewExitError(err.Error(), 1)
+		return cli.Exit(err.Error(), 1)
 	}
 	if resp != nil {
 		defer func(r *http.Response) {
@@ -93,7 +89,7 @@ func actionFunc(c *cli.Context) error {
 			}
 		}(resp)
 		if resp.StatusCode != statusCode {
-			return cli.NewExitError(fmt.Sprintf("resp code %d didn't match %d", resp.StatusCode, statusCode), 1)
+			return cli.Exit(fmt.Sprintf("resp code %d didn't match %d", resp.StatusCode, statusCode), 1)
 		}
 	}
 	return nil
